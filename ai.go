21be074c391aedd43d58347c57be5e607f3f27c0
Manu Goyal <manu.goyal2013@gmail.com>
2014-04-21 01:45:29 -0700

    Simplified the code slightly

--- ai.go
+++ ai.go
@@ -5,6 +5,8 @@ package main
 
 import (
 	"math/rand"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -16,8 +18,8 @@ type Tree struct {
 }
 
 // NewTree returns an tree with an empty grid
-func NewTree() *Tree {
-	return &Tree{NewGrid(), nil, 0, 0}
+func NewTree(g *Grid) *Tree {
+	return &Tree{g, nil, g.Score, -1}
 }
 
 // Given the height of the tree, it will fill out the tree to nodes of
@@ -29,40 +31,17 @@ func (t *Tree) Fill(height int, localRand *rand.Rand) {
 	// Generate new children
 	t.Children = make([]*Tree, 4)
 
+	t.BestScore = 0
 	for i := 0; i < 4; i++ {
-		node := &Tree{t.G.Clone(), nil, 0, 0}
+		node := NewTree(t.G.Clone())
+		// We only execute the move if tiles would be moving
 		if node.G.Move(i) {
-			// We only execute the move if tiles would be moving
 			t.Children[i] = node
-
 			if node.G.PlaceRandom(localRand) {
 				node.Fill(height-1, localRand)
 			}
-		}
-	}
-}
-
-// Given a filled tree, it takes the scores of the leaf nodes and
-// fills up the scores and directions of the parent nodes using the
-// minimax algorithm. The root of the tree will contain the best score
-// and direction
-func (t *Tree) Score() {
-	if t == nil {
-		return
-	}
-	if t.Children == nil {
-		t.BestScore = t.G.Score
-		t.BestDirection = -1
-	} else {
-		t.BestScore = 0
-		t.BestDirection = -1
-		for i, child := range t.Children {
-			if child != nil {
-				child.Score()
-				if child.BestScore > t.BestScore {
-					t.BestScore = child.BestScore
-					t.BestDirection = i
-				}
+			if node.BestScore > t.BestScore {
+				t.BestScore, t.BestDirection = node.BestScore, i
 			}
 		}
 	}
@@ -71,33 +50,20 @@ func (t *Tree) Score() {
 // Given a grid and some parameters, it figures out the next best
 // move. If it returns -1, that means it couldn't find a move.
 func (g *Grid) NextMove(height, reps, threadNum int) int {
-	directions := make(chan int)
-	bestDirection := make(chan int)
-	// This goroutine accumulates the directions from generating trees
-	// and returns the best direction on the bestDirection channel.
-	go func() {
-		counts := map[int]int{LEFT: 0, RIGHT: 0, UP: 0, DOWN: 0}
-		for dir := range directions {
-			counts[dir]++
-		}
-		maxDir, maxOcc := 0, 0
-		for direction, occurences := range counts {
-			if occurences > maxOcc {
-				maxDir, maxOcc = direction, occurences
-			}
-		}
-		bestDirection <- maxDir
-	}()
 	baseReps, leftoverReps := reps/threadNum, reps%threadNum
 	iterations := threadNum
 	if baseReps == 0 {
-		iterations = leftoverReps
+		iterations, leftoverReps = leftoverReps, 0
 	}
-	loopChan := make(chan bool, iterations)
+	// directionCounts keeps track of how many reps reported each
+	// direction as the best
+	directionCounts := make([]int64, 4)
+	var wg sync.WaitGroup
 	t := time.Now().UnixNano()
 	for i := 0; i < iterations; i++ {
 		// This goroutine creates a tree repsPerThread times and
 		// adds each resulting direction to the directions channel
+		wg.Add(1)
 		go func(i int) {
 			repsPerThread := baseReps
 			if i < leftoverReps {
@@ -105,20 +71,22 @@ func (g *Grid) NextMove(height, reps, threadNum int) int {
 			}
 			localRand := rand.New(rand.NewSource(t + int64(i)))
 			for j := 0; j < repsPerThread; j++ {
-				t := NewTree()
-				t.G = g.Clone()
+				t := NewTree(g.Clone())
 				t.Fill(height, localRand)
-				t.Score()
 				if t.BestDirection >= 0 && t.BestDirection < 4 {
-					directions <- t.BestDirection
+					atomic.AddInt64(&directionCounts[t.BestDirection], 1)
 				}
 			}
-			loopChan <- true
+			wg.Done()
 		}(i)
 	}
-	for i := 0; i < iterations; i++ {
-		<-loopChan
+	wg.Wait()
+	bestDirection := -1
+	var maxOcc int64
+	for direction, occ := range directionCounts {
+		if occ > maxOcc {
+			bestDirection, maxOcc = direction, occ
+		}
 	}
-	close(directions)
-	return <-bestDirection
+	return bestDirection
 }
